ca3f167ecaa932043ffa1bb3148f1779b39543f8
dannielchung <dannielchung@gmail.com>
2018-10-09 07:37:21 +0800

    test

--- src/main/sql/order.sql
+++ src/main/sql/order.sql
@@ -9,7 +9,7 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `categories` WRITE;
-INSERT INTO `categories` VALUES (1,'北京菜'),(2,'新疆菜'),(3,'朝鲜族菜'),(4,'四川风味菜'),(10,'上海菜'),(11,'河南菜');
+INSERT INTO `categories` VALUES (1,'麥當勞'),(2,'肯德基'),(3,'Subway'),(4,'漢堡王'),(10,'吉野家'),(11,'摩斯');
 UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `menus`;
@@ -25,7 +25,7 @@ CREATE TABLE `menus` (
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `menus` WRITE;
-INSERT INTO `menus` VALUES (6,3,'辣白菜',11.30),(9,1,'卤煮火烧',23.00),(10,2,'红柳大串',15.00),(11,4,'宫保鸡丁',20.00),(13,10,'红豆冰山',23.50),(14,10,'软炸里脊',31.00),(16,1,'北京烤鸭',98.00),(18,4,'麻婆豆腐',36.01),(19,1,'红烧猪手',26.29),(20,1,'豆汁儿',1.50);
+INSERT INTO `menus` VALUES (6,3,'起司牛肉',11.30),(9,1,'勁辣雞腿堡',23.00),(10,2,'煙燻脆培根咔啦雞腿堡-重量級XL套餐',15.00),(11,4,'莫札瑞拉雙層華堡',20.00),(13,10,'牛丼',23.50),(14,10,'豚丼',31.00),(16,1,'麥香魚',98.00),(18,4,'小華保',36.01),(19,1,'大麥克',26.29),(20,1,'麥香雞',1.50);
 UNLOCK TABLES;
 
 
